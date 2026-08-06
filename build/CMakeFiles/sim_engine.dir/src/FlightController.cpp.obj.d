81de67b3f902a85654a77b2b548011010fa62402
Josiah Christian <christianjosiah2484@gmail.com>
2026-08-05 22:01:43 -0400

    Delete build directory

--- build/CMakeFiles/sim_engine.dir/src/FlightController.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/sim_engine.dir/src/FlightController.cpp.obj: \
- C:\Users\Pam\developer_projects\AeroCPSSimulation\src\FlightController.cpp
--- build/CMakeFiles/sim_engine.dir/src/SimulatorEngine.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/sim_engine.dir/src/SimulatorEngine.cpp.obj: \
- C:\Users\Pam\developer_projects\AeroCPSSimulation\src\SimulatorEngine.cpp
