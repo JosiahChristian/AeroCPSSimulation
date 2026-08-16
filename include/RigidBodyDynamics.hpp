73e1a57d2400b5e7554f0b78aae0fa110bc62f64
Josiah Christian <christianjosiah2484@gmail.com>
2026-08-16 19:48:42 -0400

    Add include/RigidBodyDynamics.hpp

---
+++ include/RigidBodyDynamics.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Vector3.hpp"
+
+struct RigidBodyState {
+    Vector3 positionMeters;
+    Vector3 velocityMetersPerSecond;
+    Vector3 orientationRadians;
+    Vector3 angularVelocityRadiansPerSecond;
+};
+
+class RigidBodyProperties {
+public:
+    RigidBodyProperties(double massKilograms, Vector3 diagonalInertiaKilogramMetersSquared);
+
+    [[nodiscard]] double mass() const noexcept { return massKilograms_; }
+    [[nodiscard]] const Vector3& diagonalInertia() const noexcept { return diagonalInertia_; }
+
+private:
+    double massKilograms_;
+    Vector3 diagonalInertia_;
+};
+
+class RigidBodyDynamics {
+public:
+    static void advance(
+        RigidBodyState& state,
+        const RigidBodyProperties& properties,
+        Vector3 netForceWorldNewtons,
+        Vector3 netTorqueBodyNewtonMeters,
+        double timeStep);
+};
